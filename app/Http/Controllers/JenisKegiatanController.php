e91929e2eb5be8bb9daff1ef0af853ce1e6a9e6c
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-09-24 08:42:36 +0700

    [rizky] => add CRUD jenis kegiatan

--- app/Http/Controllers/JenisKegiatanController.php
+++ app/Http/Controllers/JenisKegiatanController.php
@@ -23,4 +23,74 @@ class JenisKegiatanController extends Controller
 
         return view('jenis-kegiatans.index', compact('jenisKegiatans', 'header', 'breadcrumbs'));
     }
+
+    public function create()
+    {
+        $breadcrumbs = [
+            ['name' => 'Dashboard', 'url' => route('dashboard')],
+            ['name' => 'Jenis Kegiatan', 'url' => route('jenis-kegiatans.index')],
+            ['name' => 'Create', 'url' => null],
+        ];
+
+        return view('jenis-kegiatans.create', compact('breadcrumbs'));
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama' => 'required|string|unique:jenis_kegiatan',
+        ]);
+        JenisKegiatan::create($validated);
+
+        return  redirect()->route('jenis-kegiatans.index')
+            ->with('successs', 'Jenis Kegiatan succesfully created');
+    }
+
+    public function show(JenisKegiatan $jenisKegiatan)
+    {
+        $breadcrumbs = [
+            ['name' => 'Dashboard', 'url' => route('dashboard')],
+            ['name' => 'Jenis Kegiatan', 'url' => route('jenis-kegiatans.index')],
+            ['name' => 'Show', 'url' => null],
+        ];
+
+        $fields = [
+            ['label' => 'ID', 'value' => $jenisKegiatan->id],
+            ['label' => 'Nama', 'value' => $jenisKegiatan->nama]
+        ];
+
+        return view('jenis-kegiatans.show', compact('breadcrumbs', 'fields'));
+    }
+
+    public function edit(JenisKegiatan $jenisKegiatan)
+    {
+        $breadcrumbs = [
+            ['name' => 'Dashboard', 'url' => route('dashboard')],
+            ['name' => 'Jenis Kegiatan', 'url' => route('jenis-kegiatans.index')],
+            ['name' => 'Edit', 'url' => null],
+        ];
+
+        return view('jenis-kegiatans.edit', compact('breadcrumbs'));
+    }
+
+    public function update(Request $request, JenisKegiatan $jenisKegiatan)
+    {
+        $validated = $request->validate([
+            'nama' =>   'required|string|unique:jenis_kegiatans|min:3'
+        ]);
+
+        $jenisKegiatan->update($validated);
+
+        return redirect()->route('jenis-kegiatans.index')
+            ->with('success', 'Jenis Kegiatan updated succesfully');
+    }
+
+    public function destroy(JenisKegiatan $jenisKegiatan)
+    {
+        $jenisKegiatan->delete();
+
+        return  redirect()->route('jenis-kegiatans.index')->with('success', 'Jenis Kegiatan deleted succesfully');
+    }
+
+
 }
